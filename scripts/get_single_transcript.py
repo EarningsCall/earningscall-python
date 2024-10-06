3d9e38a0634c533200ae46f746d46c0ea18dbbc6
EarningsCall <dev@earningscall.biz>
2024-10-06 13:09:01 -0500

    Add new features.
    * Don't throw an exception when audio file is missing, just return `None`
    * Throw useful error in the case of not authorized to download Audio
      Files.
    * Add advanced transcript data.
    * Add more unit test coverage.

--- scripts/get_single_transcript.py
+++ scripts/get_single_transcript.py
@@ -3,5 +3,5 @@ from earningscall import get_company
 
 company = get_company("msft")
 
-transcript = company.get_transcript(year=2021, quarter=3)
+transcript = company.get_transcript(year=2021, quarter=3, level=4)
 print(f"{company} Q3 2021 Transcript Text: \"{transcript.text[:100]}...\"")
--- tests/test_get_transcript.py
+++ tests/test_get_transcript.py
@@ -1,10 +1,13 @@
+import earningscall
 import pytest
 import responses
 
 from earningscall import get_company
 from earningscall.api import purge_cache
+from earningscall.company import Company
 from earningscall.errors import InsufficientApiAccessError
-from earningscall.symbols import clear_symbols
+from earningscall.event import EarningsEvent
+from earningscall.symbols import clear_symbols, CompanyInfo
 from earningscall.utils import data_path
 
 
@@ -16,11 +19,48 @@ from earningscall.utils import data_path
 #     requests.get("https://v2.api.earningscall.biz/transcript?apikey=demo&exchange=NASDAQ&symbol=MSFT&year=2023&quarter=1")
 
 
+@pytest.fixture(autouse=True)
+def run_before_and_after_tests():
+    """Fixture to execute asserts before and after a test is run"""
+    # Setup: fill with any logic you want
+    earningscall.api_key = None
+    purge_cache()
+    clear_symbols()
+    yield  # this is where the testing happens
+    # Teardown : fill with any logic you want
+    earningscall.api_key = None
+
+
+@responses.activate
+def test_get_transcript_invalid_inputs():
+    ##
+    responses._add_from_file(file_path=data_path("symbols-v2.yaml"))
+    responses._add_from_file(file_path=data_path("msft-transcript-response.yaml"))
+    ##
+    company = get_company("msft")
+    ##
+    with pytest.raises(ValueError):
+        company.get_transcript(year=2023)
+    with pytest.raises(ValueError):
+        company.get_transcript(quarter=1)
+    with pytest.raises(ValueError):
+        company.get_transcript(quarter=0)
+    with pytest.raises(ValueError):
+        company.get_transcript(year=2023, quarter=5)
+    with pytest.raises(ValueError):
+        company.get_transcript(year=2023, quarter=1, level=0)
+    with pytest.raises(ValueError):
+        company.get_transcript(year=2023, quarter=1, level=5)
+    ##
+    invalid_company = Company(company_info=CompanyInfo())
+    transcript = invalid_company.get_transcript(year=2023, quarter=1)
+    ##
+    assert transcript is None
+
+
 @responses.activate
 def test_get_demo_company():
     ##
-    purge_cache()
-    clear_symbols()
     responses._add_from_file(file_path=data_path("symbols-v2.yaml"))
     responses._add_from_file(file_path=data_path("msft-transcript-response.yaml"))
     ##
@@ -35,20 +75,105 @@ def test_get_demo_company():
 @responses.activate
 def test_get_demo_company_with_event_populated():
     ##
-    purge_cache()
-    clear_symbols()
     responses._add_from_file(file_path=data_path("symbols-v2.yaml"))
     responses._add_from_file(file_path=data_path("demo-symbols-v2-alpha.yaml"))
     ##
     company = get_company("aapl")
     ##
-    transcript = company.get_transcript(year=2022, quarter=1)
+    transcript = company.get_transcript(event=EarningsEvent(year=2022, quarter=1))
     assert transcript.event.year == 2022
     assert transcript.event.quarter == 1
     assert transcript.event.conference_date.isoformat() == "2022-01-19T00:00:00-08:00"
     assert transcript.text[:100] == (
         "Good day and welcome to the Apple Q1 Fiscal Year 2021 earnings conference " "call. Today's call is bein"
     )
+
+
+@responses.activate
+def test_get_demo_company_with_advanced_transcript_data():
+    ##
+    responses._add_from_file(file_path=data_path("symbols-v2.yaml"))
+    responses._add_from_file(file_path=data_path("aapl-q1-2022-advanced-data-level-2.yaml"))
+    ##
+    company = get_company("aapl")
+    ##
+    transcript = company.get_transcript(year=2022, quarter=1, level=2)
+    ##
+    assert transcript.event.year == 2022
+    assert transcript.event.quarter == 1
+    assert transcript.event.conference_date.isoformat() == "2022-01-27T14:00:00-08:00"
+    assert transcript.text[:100] == (
+        "Good day and welcome to the Apple Q1 FY 2022 earnings conference call. Today's call is being recorde"
+    )
+    assert transcript.speakers[0].speaker == "spk03"
+    assert transcript.speakers[0].words is None
+    assert transcript.speakers[0].start_times is None
+
+
+@responses.activate
+def test_get_demo_company_with_advanced_transcript_data_level_3():
+    ##
+    responses._add_from_file(file_path=data_path("symbols-v2.yaml"))
+    responses._add_from_file(file_path=data_path("aapl-q1-2022-advanced-data-level-3.yaml"))
+    ##
+    company = get_company("aapl")
+    ##
+    transcript = company.get_transcript(year=2022, quarter=1, level=3)
+    ##
+    assert transcript.event.year == 2022
+    assert transcript.event.quarter == 1
+    assert transcript.event.conference_date.isoformat() == "2022-01-27T14:00:00-08:00"
+    assert transcript.text[:100] == (
+        "Good day and welcome to the Apple Q1 FY 2022 earnings conference call. Today's call is being recorde"
+    )
+    assert transcript.speakers[0].speaker == "spk03"
+    assert " ".join(transcript.speakers[0].words)[:100] == (
+        "Good day and welcome to the Apple Q1 FY 2022 earnings conference call. Today's call is being recorde"
+    )
+    assert transcript.speakers[0].start_times[:7] == [
+        0.029,
+        0.189,
+        0.429,
+        0.57,
+        0.91,
+        1.03,
+        1.27,
+    ]
+
+
+@responses.activate
+def test_get_demo_company_with_advanced_transcript_data_level_4():
+    ##
+    responses._add_from_file(file_path=data_path("symbols-v2.yaml"))
+    responses._add_from_file(file_path=data_path("aapl-q1-2022-advanced-data-level-4.yaml"))
+    ##
+    company = get_company("aapl")
+    ##
+    transcript = company.get_transcript(year=2022, quarter=1, level=4)
+    ##
+    assert transcript.event.year == 2022
+    assert transcript.event.quarter == 1
+    assert transcript.event.conference_date.isoformat() == "2022-01-27T14:00:00-08:00"
+    assert transcript.text[:100] == (
+        "Good day and welcome to the Apple Q1 FY 2022 earnings conference call. Today's call is being recorde"
+    )
+    assert transcript.prepared_remarks[:100] == (
+        "Good day and welcome to the Apple Q1 FY 2022 earnings conference call. Today's call is being recorde"
+    )
+    assert transcript.questions_and_answers[:100] == (
+        "We'll take our first question from Katie Huberty with Morgan Stanley. Caller, please check your mute"
+    )
+
+    assert transcript.speakers is None
+
+
+@responses.activate
+def test_get_non_demo_company():
+    ##
+    responses._add_from_file(file_path=data_path("demo-symbols-v2.yaml"))
+    ##
+    with pytest.raises(InsufficientApiAccessError):
+        get_company("nvda")
 
 
 # Uncomment and run following code to generate demo-symbols-v2.yaml file
@@ -69,18 +194,8 @@ def test_get_demo_company_with_event_populated():
 # from responses import _recorder
 #
 #
-# @_recorder.record(file_path="demo-symbols-v2-alpha.yaml")
+# @_recorder.record(file_path="aapl-q1-2022-advanced-data-level-4.yaml")
 # def test_save_symbols_v1():
-#     requests.get("https://v2.api.alpha.earningscall.biz/transcript?apikey=demo&exchange=NASDAQ&symbol=AAPL&year=2022&quarter=q1")
-#
-
-
-@responses.activate
-def test_get_non_demo_company():
-    ##
-    purge_cache()
-    clear_symbols()
-    responses._add_from_file(file_path=data_path("demo-symbols-v2.yaml"))
-    ##
-    with pytest.raises(InsufficientApiAccessError):
-        get_company("nvda")
+#     requests.get(
+#         "https://v2.api.earningscall.biz/transcript?apikey=demo&exchange=NASDAQ&symbol=AAPL&year=2022&quarter=1&level=4"
+#     )
