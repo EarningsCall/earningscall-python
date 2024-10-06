3e1cbdfb27f1d97159588c83f915a20e4a3193de
EarningsCall <dev@earningscall.biz>
2024-10-06 15:17:37 -0500

    Add test coverage.

---
+++ tests/test_company.py
@@ -0,0 +1,32 @@
+import json
+
+import pytest
+
+from earningscall.company import Company
+from earningscall.symbols import CompanyInfo
+
+
+def test_company_constructor_checks():
+    with pytest.raises(ValueError):
+        Company(company_info=None)
+
+
+def test_company_to_string():
+    company_info = CompanyInfo(
+        name="Test Company",
+        symbol="TEST",
+        exchange="TEST",
+        sector="TEST",
+        industry="TEST",
+    )
+    company = Company(company_info=company_info)
+    assert str(company) == "Test Company"
+    assert company.company_info == company_info
+    assert json.loads(company_info.to_json()) == {
+        "name": "Test Company",
+        "symbol": "TEST",
+        "exchange": "TEST",
+        "sector": "TEST",
+        "industry": "TEST",
+    }
+    assert company_info.exchange_symbol() == "TEST_TEST"
