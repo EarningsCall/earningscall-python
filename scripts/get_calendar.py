0af27226296bd9f3d4b67218d84e7f1eda49426a
EarningsCall <dev@earningscall.biz>
2025-02-12 13:16:45 -0600

    Cleanup example script.

--- scripts/get_calendar.py
+++ scripts/get_calendar.py
@@ -1,15 +1,12 @@
-from datetime import datetime
+from datetime import date
 import earningscall  # noqa: F401
 
 from earningscall import get_calendar
 
-
 # TODO: Set your API key here:
 # earningscall.api_key = "YOUR API KEY HERE"
 
-date = datetime(2025, 1, 10)
-
-events = get_calendar(date)
+events = get_calendar(date(2025, 1, 10))
 
 for event in events:
     print(
